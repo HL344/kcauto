db4a09aa0d8b31cd2b4d4d867c500dd2e8644284
HL344 <hl.user.344@tutanota.com>
2022-03-17 00:25:11 +0100

    add E1 english

--- autostart-kcauto.sh
+++ autostart-kcauto.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-chromium --remote-debugging-port=9222 http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/ >/dev/null 2>&1  &
-sleep 120
+perl -pi -e 's/exit_type\":\"Crashed/exit_type\":\"none/' ~/.config/google-chrome/Default/Preferences
+chromium --disable-session-crashed-bubble --remote-debugging-port=9222  http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/ >/dev/null 2>&1  &
+sleep 45
 cd /home/elias/Documents/kcauto
 gnome-terminal --title=kcautostart -- bash -c "./kcloop.sh; exec bash"
 xdotool windowminimize $(xdotool search --name kcautostart|head -1)
